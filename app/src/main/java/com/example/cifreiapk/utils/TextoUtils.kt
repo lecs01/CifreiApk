574338a1892f3dee8eb7f9530e722555d74b8021
Esdras Bezerra da Silva <esdrasilva@gmail.com>
2020-11-18 18:03:47 -0300

    Inclusao de espacamento entre linhas e evento de botões para inserção de notas.

---
+++ app/src/main/java/com/example/cifreiapk/utils/TextoUtils.kt
@@ -0,0 +1,37 @@
+package com.example.cifreiapk.utils
+
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+
+class TextoUtils {
+    companion object {
+        fun convertWithBlankLines(text: String): String {
+            val inputStream = ByteArrayInputStream(text.toByteArray())
+            val reader = BufferedReader(inputStream.reader())
+            val content = StringBuilder()
+            try {
+                var line = reader.readLine()
+                var sizeLine = line.length
+                while (line != null) {
+                    content.append(fillBlankLine(sizeLine))
+                    content.append("\n")
+                    content.append(line)
+                    line = reader.readLine()
+                    content.append("\n")
+                }
+                return content.toString()
+            } finally {
+                inputStream.close()
+                reader.close()
+            }
+        }
+
+        fun fillBlankLine(size: Int) : String{
+            var blankLine = " "
+            for (x in 0 until size) {
+                blankLine += "  "
+            }
+            return blankLine
+        }
+    }
+}
