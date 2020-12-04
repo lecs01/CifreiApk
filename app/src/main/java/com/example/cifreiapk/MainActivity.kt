36aef84b0f77c33f8b6918a53861fc2efd0e70e0
lecs01 <lecs.01@outlook.com>
2020-12-03 22:49:00 -0300

    criacao do RecyclerView;

--- app/src/main/java/com/example/cifreiapk/MainActivity.kt
+++ app/src/main/java/com/example/cifreiapk/MainActivity.kt
@@ -1,14 +1,72 @@
 package com.example.cifreiapk
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cifreiapk.model.Cifra
+import com.example.cifreiapk.model.cifraAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: cifraAdapter
+    private lateinit var mbase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Create a instance of the database and get
+        // its reference
+        // Create a instance of the database and get
+        // its reference
+        mbase = FirebaseDatabase.getInstance().reference
+
+        recyclerView = findViewById(R.id.recyclerViewCifras)
+
+        // To display the Recycler view linearly
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+            LinearLayoutManager(this))
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        val options: FirebaseRecyclerOptions<Cifra> = FirebaseRecyclerOptions.Builder<Cifra>()
+            .setQuery(mbase, Cifra::class.java)
+            .build()
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = cifraAdapter(options)
+        // Connecting Adapter class with the Recycler view*/
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter)
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stoping of the activity
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 
     fun criarCifra(view: View) {
---
+++ app/src/main/java/com/example/cifreiapk/model/cifraAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.cifreiapk.model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cifreiapk.R
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+
+class cifraAdapter(
+    options: FirebaseRecyclerOptions<Cifra>,
+) : FirebaseRecyclerAdapter<Cifra, cifraAdapter.cifrasViewholder>(options) {
+    override fun onBindViewHolder(
+        holder: cifrasViewholder,
+        position: Int, model: Cifra,
+    ) {
+        holder.tituloDaMusica.setText(model.tituloDaMusica.toString())
+        holder.nomeDoArtista.setText(model.nomeDoArtista.toString())
+        holder.letraDaMusica.setText(model.letraDaMusica.toString())
+        holder.nTom.setText(model.nTom.toString())
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): cifrasViewholder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_cifras, parent, false) as TextView
+        return cifrasViewholder(view)
+    }
+
+    inner class cifrasViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var tituloDaMusica: TextView
+        lateinit var nomeDoArtista: TextView
+        lateinit var letraDaMusica: TextView
+        lateinit var nTom: TextView
+
+        init {
+            tituloDaMusica = itemView.findViewById(R.id.textViewTitulo)
+            nomeDoArtista = itemView.findViewById(R.id.textViewArtista)
+            letraDaMusica = itemView.findViewById(R.id.editViewLetra)
+            nTom = itemView.findViewById(R.id.textViewTom)
+        }
+    }
+}
